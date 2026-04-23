22721b0a12e952bb4e403bce15d5cdf5b43b3e10
bazarkinzakhar <bazarkinzakhar@gmail.com>
2026-04-23 03:21:41 +0300

    feat: добавлен NAT gateway

---
+++ terraform/modules/vpc/main.tf
@@ -0,0 +1,26 @@
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnets)
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.private_subnets[count.index]
+  availability_zone = var.azs[count.index]
+
+  tags = { Name = "${var.env_name}-private-${count.index + 1}" }
+}
+
+# NAT Gateway
+resource "aws_eip" "nat" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.this.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.id
+  }
+}
